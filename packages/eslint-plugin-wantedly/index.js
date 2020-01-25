9ad9a42c5159bc1a0862fb2f79f5e655a00fe446
Yuki Yamada <yamadayuuki0818@gmail.com>
2020-01-25 20:06:17 +0900

    Create NexusCamelCaseFieldName rule

--- packages/eslint-plugin-wantedly/index.js
+++ packages/eslint-plugin-wantedly/index.js
@@ -1,11 +1,13 @@
 const GraphQLOperationName = require("./rules/GraphQLOperationName");
 const GraphQLPascalCaseTypeName = require("./rules/GraphQLPascalCaseTypeName");
+const NexusCamelCaseFieldName = require("./rules/NexusCamelCaseFieldName");
 const NexusPascalCaseTypeName = require("./rules/NexusPascalCaseTypeName");
 
 module.exports = {
   rules: {
     [GraphQLOperationName.RULE_NAME]: GraphQLOperationName.RULE,
     [GraphQLPascalCaseTypeName.RULE_NAME]: GraphQLPascalCaseTypeName.RULE,
+    [NexusCamelCaseFieldName.RULE_NAME]: NexusCamelCaseFieldName.RULE,
     [NexusPascalCaseTypeName.RULE_NAME]: NexusPascalCaseTypeName.RULE,
   },
 };
