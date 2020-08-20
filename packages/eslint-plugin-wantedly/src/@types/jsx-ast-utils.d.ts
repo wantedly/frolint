547feafebd57b85c20fdcdab28ea1c10e3e5aab2
Tsuyoshi HARA <chloe463+git@gmail.com>
2020-08-20 17:14:13 +0900

    build(deps-dev): Remove jsx-ast-utils

--- packages/eslint-plugin-wantedly/src/@types/jsx-ast-utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { TSESTree } from "@typescript-eslint/experimental-utils";
-
-declare module "jsx-ast-utils" {
-  export function hasProp(element: TSESTree.JSXAttribute[], name: string): boolean;
-}
