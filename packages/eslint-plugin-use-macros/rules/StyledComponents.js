8b8fe7d2725e2ef7e9e699777a5f2f5259884173
Tsuyoshi HARA <chloe463+git@gmail.com>
2024-09-12 16:28:08 +0900

    change(eslint-plugin-use-macros): Make it possible to work on eslint v9

--- packages/eslint-plugin-use-macros/rules/StyledComponents.js
+++ packages/eslint-plugin-use-macros/rules/StyledComponents.js
@@ -1,9 +1,15 @@
 const createUseMacro = require("../createUseMacro");
 
+/** @type {import('eslint').Rule.RuleModule } */
 module.exports = {
   meta: {
     type: "suggestion",
     fixable: "code",
+    schema: [
+      {
+        enum: ["error", "warn", "off"],
+      },
+    ],
   },
   create(context) {
     return {
