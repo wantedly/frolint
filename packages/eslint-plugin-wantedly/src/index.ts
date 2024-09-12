affa8cd9942a6beea5f47e71188e297cf13e1643
Tsuyoshi HARA <chloe463+git@gmail.com>
2024-09-12 15:40:30 +0900

    change(eslint-plugin-wantedly): Make nexus-field-description possible to work on v9

--- packages/eslint-plugin-wantedly/src/index.ts
+++ packages/eslint-plugin-wantedly/src/index.ts
@@ -2,7 +2,7 @@ import * as GRAPHQL_OPERATION_NAME from "./rules/graphql-operation-name";
 import * as GRAPHQL_PASCAL_CASE_TYPE_NAME from "./rules/graphql-pascal-case-type-name";
 import * as NEXUS_CAMEL_CASE_FIELD_NAME from "./rules/nexus-camel-case-field-name";
 import * as NEXUS_ENUM_VALUES_DESCRIPTION from "./rules/nexus-enum-values-description";
-// import * as NEXUS_FIELD_DESCRIPTION from "./rules/nexus-field-description";
+import * as NEXUS_FIELD_DESCRIPTION from "./rules/nexus-field-description";
 // import * as NEXUS_PASCAL_CASE_TYPE_NAME from "./rules/nexus-pascal-case-type-name";
 // import * as NEXUS_TYPE_DESCRIPTION from "./rules/nexus-type-description";
 // import * as NEXUS_UPPER_CASE_ENUM_MEMBERS from "./rules/nexus-upper-case-enum-members";
@@ -29,5 +29,6 @@ export const plugins: ESLint.Plugin = {
     [GRAPHQL_PASCAL_CASE_TYPE_NAME.RULE_NAME]: GRAPHQL_PASCAL_CASE_TYPE_NAME.RULE,
     [NEXUS_CAMEL_CASE_FIELD_NAME.RULE_NAME]: NEXUS_CAMEL_CASE_FIELD_NAME.RULE,
     [NEXUS_ENUM_VALUES_DESCRIPTION.RULE_NAME]: NEXUS_ENUM_VALUES_DESCRIPTION.RULE,
+    [NEXUS_FIELD_DESCRIPTION.RULE_NAME]: NEXUS_FIELD_DESCRIPTION.RULE,
   },
 };
